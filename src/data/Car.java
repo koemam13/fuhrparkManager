0fdbd47f950a9c63bc77631fc26f310b3c17e216
imperatus <imperatus@Imperatus>
2018-07-24 09:41:03 +0200

    fkrst

---
+++ src/data/Car.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.GregorianCalendar;
+
+
+/**
+ *
+ * @author imperatus
+ */
+public class Car
+{
+  private final String name;
+  private final String id;
+  private final String kostenstelle;
+  private final int km;
+  private final String firstRegistration;
+  private final String lastInspection;
+  private final String nextInspection;
+  private final String lastService;
+  private final String nextService;
+  private final double cost;
+
+
+  public Car (String name, String id, String kostenstelle, int km, String firstRegistration, String lastInspection, String nextInspection, String lastService, String nextService, double cost)
+  {
+    this.name = name;
+    this.id = id;
+    this.kostenstelle = kostenstelle;
+    this.km = km;
+    this.firstRegistration = firstRegistration;
+    this.lastInspection = lastInspection;
+    this.nextInspection = nextInspection;
+    this.lastService = lastService;
+    this.nextService = nextService;
+    this.cost = cost;
+  }
+
+
+  public String getName ()
+  {
+    return name;
+  }
+
+
+  public String getId ()
+  {
+    return id;
+  }
+
+
+  public String getKostenstelle ()
+  {
+    return kostenstelle;
+  }
+
+
+  public int getKm ()
+  {
+    return km;
+  }
+
+
+  public String getFirstRegistration ()
+  {
+    return firstRegistration;
+  }
+
+
+  public String getLastInspection ()
+  {
+    return lastInspection;
+  }
+
+
+  public String getNextInspection ()
+  {
+    return nextInspection;
+  }
+
+
+  public String getLastService ()
+  {
+    return lastService;
+  }
+
+
+  public String getNextService ()
+  {
+    return nextService;
+  }
+
+
+  public double getCost ()
+  {
+    return cost;
+  }
+  
+  public void write (Writer w) throws IOException 
+  {
+    w.write(name);
+    w.write(",");
+    w.write(id);
+    w.write(",");
+    w.write(kostenstelle);
+    w.write(",");
+    w.write(String.valueOf(km));
+    w.write(",");
+    w.write(firstRegistration);
+    w.write(",");
+    w.write(lastInspection);
+    w.write(",");
+    w.write(nextInspection);
+    w.write(",");
+    w.write(lastService);
+    w.write(",");
+    w.write(nextService);
+    w.write(",");
+    w.write(String.valueOf(cost));
+    w.flush();
+    }
+  
+ 
+}
---
+++ src/tableModel/tableModel.java
@@ -0,0 +1,149 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tableModel;
+
+import data.Car;
+import data.FileFormatException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+
+/**
+ *
+ * @author imperatus
+ */
+public class tableModel extends AbstractTableModel
+{
+  private final List<Car> cars = new ArrayList<>();
+  
+  private static final String [] colNames = {"Fahrzeug","Kennzeichen","Kostenstelle","Kilometer","Erst Zulassung",
+                                             "Lletzte §57a Begutachtung", "Nächste Begutachtung","Letzter Service",
+                                             "Nächster Service", "Kosten"};
+
+  @Override
+  public int getRowCount ()
+  {
+    return cars.size();
+  }
+
+
+  @Override
+  public int getColumnCount ()
+  {
+    return colNames.length;
+  }
+
+
+  @Override
+  public String getColumnName (int columnIndex)
+  {
+    return colNames[columnIndex];
+  }
+  
+  
+
+
+  @Override
+  public Object getValueAt (int rowIndex, int columnIndex)
+  {
+    final Car c = cars.get(rowIndex);
+    switch(columnIndex)
+    {
+      case 0: 
+        return c.getName();
+      case 1:
+        return c.getId();
+      case 2:
+        return c.getKostenstelle();
+      case 3:
+        return c.getKm();
+      case 4:
+        return c.getFirstRegistration();
+      case 5:
+        return c.getLastInspection();
+      case 6:
+        return c.getNextInspection();
+      case 7:
+        return c.getLastService();
+      case 8:
+        return c.getNextService();
+      case 9:
+        return c.getCost();
+    }
+    return null;
+  }
+  
+  public boolean add (Car c)
+  {
+    boolean rv = cars.add(c);
+    fireTableRowsInserted(cars.size()-1, cars.size()-1);
+    return rv;
+  }
+  
+  public boolean remove(Car c)
+  {
+    int index = cars.indexOf(c);
+    boolean rv = cars.remove(c);
+    fireTableRowsDeleted(index, index);
+    return rv;
+    
+  }
+  
+  public Car remove (int index)
+  {
+    Car c = cars.remove(index);
+    fireTableRowsDeleted(index, index);
+    return c;
+    
+  }
+  
+  public Car getCar (int index)
+  {
+    Car c = cars.get(index);
+    return c;
+  }
+  
+  public void write (BufferedWriter w) throws IOException
+  {
+    for (Car c : cars) {
+      c.write(w);
+      w.newLine();
+    }
+  }
+  
+    public void set (int index, Car c)
+  {
+    cars.set(index, c);
+    fireTableRowsUpdated(index, index);
+  }
+
+  
+  public void read (BufferedReader r) throws FileFormatException, IOException
+  {
+    cars.clear();
+    fireTableDataChanged();
+    
+    while (r.ready()) {
+      String line = r.readLine().trim();
+      
+      String s[] = line.split(",");
+      
+      if(s.length < 9)
+        throw new FileFormatException("Falsches format");
+      
+      System.out.println(java.util.Arrays.toString(s));
+      
+      cars.add(new Car(s[0], s[1],s[2],Integer.parseInt(s[3]),s[4],s[5],s[6],s[7],s[8],Double.parseDouble(s[9])));
+      fireTableDataChanged();
+      
+    }
+  }
+  
+}
